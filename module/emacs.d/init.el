a252998e9dc4b2e06a76eafe6f7acede87b5d0af
Alex Kyoungdon Jang <alex.jang@samsung.com>
2015-12-24 22:35:22 +0900

    Fix emacs config error
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- module/emacs.d/init.el
+++ module/emacs.d/init.el
@@ -51,8 +51,3 @@
      (340 . "#f0c674")
      (360 . "#b5bd68"))))
  '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.

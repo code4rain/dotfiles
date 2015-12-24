c5485bb2d52261148058b630a70d73d7e0a7d1a2
Alex Kyoungdon Jang <alex.jang@samsung.com>
2015-12-24 22:34:10 +0900

    update emacs init.el
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- module/emacs.d/init.el
+++ module/emacs.d/init.el
@@ -56,6 +56,3 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight semi-bold :height 105 :width normal))))
- '(cursor ((t (:background "firebrick")))))
-

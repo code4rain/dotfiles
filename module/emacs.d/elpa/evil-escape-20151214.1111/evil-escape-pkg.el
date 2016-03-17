08d5348557bf5c4e83d83247e404afa160aee093
Alex Kyoungdon Jang <alex.jang@samsung.com>
2016-03-17 12:19:16 +0900

    emacs: remove previous packages
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- module/emacs.d/elpa/evil-escape-20151214.1111/evil-escape-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-escape" "20151214.1111" "Escape from anything with a customizable key sequence" '((emacs "24") (evil "1.0.9") (cl-lib "0.5")) :url "https://github.com/syl20bnr/evil-escape" :keywords '("convenience" "editing" "evil"))
--- module/emacs.d/elpa/toc-org-20150921.705/toc-org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "toc-org" "20150921.705" "add table of contents to org-mode files (formerly, org-toc)" 'nil :url "https://github.com/snosov1/toc-org" :keywords '("org-mode" "org-toc" "toc-org" "org" "toc" "table" "of" "contents"))

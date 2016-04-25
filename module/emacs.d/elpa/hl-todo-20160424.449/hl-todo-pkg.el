947c6d6cdeb099007fbb74519f8ecf545632f314
Alex Kyoungdon Jang <alex.jang@samsung.com>
2016-04-25 20:36:48 +0900

    emacs:update package
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

---
+++ module/emacs.d/elpa/hl-todo-20160424.449/hl-todo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hl-todo" "20160424.449" "highlight TODO keywords" 'nil :url "http://github.com/tarsius/hl-todo" :keywords '("convenience"))
--- module/emacs.d/elpa/web-mode-20160418.109/web-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "web-mode" "../../../../../.emacs.d/elpa/web-mode-20160418.109/web-mode.el"
-;;;;;;  "ec886d7f181ccd0b0c5695fa13e38fee")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/web-mode-20160418.109/web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/web-mode-20160418.109/web-mode-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/web-mode-20160418.109/web-mode.el")
-;;;;;;  (22296 32258 649740 929000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; web-mode-autoloads.el ends here

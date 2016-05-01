096a07f46bb3465d4d509a432c102f0a45fe8094
Alex Kyoungdon Jang <alex.jang@samsung.com>
2016-05-01 22:32:07 +0900

    bin:add nvim install script
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

---
+++ bin/scripts/nvim_install.sh
@@ -0,0 +1,17 @@
+!# /usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+    echo "Sorry, you are not root."
+    exit 1
+fi
+
+apt-get install python-dev python-pip python3-dev python3-pip
+add-apt-repository ppa:neovim-ppa/unstable
+apt-get update
+apt-get install neovim
+update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+update-alternatives --config vi
+update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+update-alternatives --config vim
+update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+update-alternatives --config editor

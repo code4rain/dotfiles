7a21c11bbdc56f4cc9e12c59c6cb5aa0bc9a7443
Alex Kyoungdon Jang <alex.jang@samsung.com>
2016-05-09 00:26:09 +0900

    emacs: add tumblesocks
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

---
+++ module/emacs.d/elpa/oauth-20130127.1751/oauth-autoloads.el
@@ -0,0 +1,19 @@
+;;; oauth-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/oauth-20130127.1751/hmac-sha1.el"
+;;;;;;  "../../../../../.emacs.d/elpa/oauth-20130127.1751/oauth-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/oauth-20130127.1751/oauth-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/oauth-20130127.1751/oauth.el")
+;;;;;;  (22319 18015 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; oauth-autoloads.el ends here
---
+++ module/emacs.d/elpa/tumblesocks-20140215.1247/tumblesocks-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tumblesocks" "20140215.1247" "An Emacs tumblr client."
+  '((htmlize "1.39")
+    (oauth "1.0.3")
+    (markdown-mode "1.8.1")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

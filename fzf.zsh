3ae3e3ed99104b0c0f7248832f70f51ff8ce9782
Alex Kyoungdon Jang <alex.jang@samsung.com>
2016-05-10 10:45:15 +0900

    update it!
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- fzf.zsh
+++ fzf.zsh
@@ -1,20 +1,20 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/alex/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/alex/.fzf/bin"
+if [[ ! "$PATH" == */home/alex.jang/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/alex.jang/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */home/alex/.fzf/man* && -d "/home/alex/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/alex/.fzf/man"
+if [[ ! "$MANPATH" == */home/alex.jang/.fzf/man* && -d "/home/alex.jang/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/alex.jang/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/alex/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/alex.jang/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/alex/.fzf/shell/key-bindings.zsh"
+source "/home/alex.jang/.fzf/shell/key-bindings.zsh"
 

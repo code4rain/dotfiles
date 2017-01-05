e8042a750c8b4212d96f81dedcc89241a2541527
Alex Kyoungdon Jang <alex.jang@samsung.com>
2017-01-05 21:39:08 +0900

    fzf:update fzf
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- fzf.zsh
+++ fzf.zsh
@@ -4,12 +4,6 @@ if [[ ! "$PATH" == */home/alex.jang/.fzf/bin* ]]; then
   export PATH="$PATH:/home/alex.jang/.fzf/bin"
 fi
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */home/alex.jang/.fzf/man* && -d "/home/alex.jang/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/alex.jang/.fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/home/alex.jang/.fzf/shell/completion.zsh" 2> /dev/null

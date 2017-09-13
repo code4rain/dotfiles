d3b42421920436bf547a11a88b487233eb119ee1
Alex Kyoungdon Jang <alex.jang@samsung.com>
2017-09-13 23:08:17 +0900

    script: update neovim install script
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- bin/scripts/nvim_install.sh
+++ bin/scripts/nvim_install.sh
@@ -1,4 +1,4 @@
-!# /usr/bin/env bash
+#! /usr/bin/env bash
 
 if [ "$(id -u)" != "0" ]; then
     echo "Sorry, you are not root."
@@ -9,6 +9,7 @@ apt-get install python-dev python-pip python3-dev python3-pip
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
 apt-get install neovim
+pip3 install neovim
 update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 update-alternatives --config vi
 update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60

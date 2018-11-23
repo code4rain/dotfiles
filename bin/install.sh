7b973c99c068257ce3debaecc25531c7b0f0a3e5
Alex Kyoungdon Jang <alex.jang@samsung.com>
2018-11-23 13:50:19 +0900

    bin: update install path
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- bin/install.sh
+++ bin/install.sh
@@ -6,15 +6,15 @@ OH_MY_ZSH_GITHUB="https://github.com/robbyrussell/oh-my-zsh.git"
 OH_MY_ZSH_AUTOSUGGESTION_DIR="$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 OH_MY_ZSH_AUTOSUGGESTION_GITHUB="https://github.com/zsh-users/zsh-autosuggestions.git"
 
-TIG_DIR="$HOME/@WORK/tig"
+TIG_DIR="$HOME/External/tig"
 TIG_GITHUB="https://github.com/jonas/tig.git"
 TIG_BUILD="sudo apt-get install -y automake libncurses5-dev && sh autogen.sh && echo 'configure' && ./configure && echo 'make' && make -j32 && echo 'systemwide install' && sudo make install"
 
-RANGER_DIR="$HOME/@WORK/ranger"
+RANGER_DIR="$HOME/External/ranger"
 RANGER_GITHUB="https://github.com/hut/ranger.git"
 RANGER_BUILD="sudo apt-get install -y automake libncurses5-dev && sudo make install"
 
-TMUX_DIR="$HOME/@WORK/tmux"
+TMUX_DIR="$HOME/External/tmux"
 TMUX_GITHUB="https://github.com/ThomasAdam/tmux.git"
 TMUX_BUILD="sudo apt-get install -y libevent-dev && sh autogen.sh && echo 'configure' && ./configure && echo 'make' && make -j32 && echo 'systemwide install' && sudo make install"
 
